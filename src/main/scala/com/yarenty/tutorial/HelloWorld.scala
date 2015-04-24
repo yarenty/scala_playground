1a048c19e51ebde039ce9369554948c37a354937
yarenty <yarenty@gmail.com>
2015-04-24 12:01:28 +0100

    just hello world

---
+++ src/main/scala/com/yarenty/tutorial/HelloWorld.scala
@@ -0,0 +1,12 @@
+package com.yarenty.tutorial
+
+/**
+ * Everyone need to do this ;-).
+ * Created by yarenty on 24/04/15.
+ */
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    println("Hello World")
+  }
+}
