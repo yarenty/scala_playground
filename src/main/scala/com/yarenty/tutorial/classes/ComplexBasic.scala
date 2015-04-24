fc8ab35545ef41b8aa8efb4c7c5dbf177cd0fb5a
yarenty <yarenty@gmail.com>
2015-04-24 12:27:33 +0100

    basic class definition with params

---
+++ src/main/scala/com/yarenty/tutorial/classes/ComplexBasic.scala
@@ -0,0 +1,14 @@
+package com.yarenty.tutorial.classes
+
+
+/**
+ * Initial version of complex.
+ * Classes could have parameters (liks initializers in java).
+ * Created by yarenty on 24/04/15.
+ */
+class ComplexBasic(real: Double, imaginary: Double) {
+
+  def re() = real
+
+  def im() = imaginary
+}
