643896f0cb44c5e1d0540a157bb122fe58f46238
yarenty <yarenty@gmail.com>
2015-04-24 12:04:45 +0100

    simple java usage

---
+++ src/main/scala/com/yarenty/tutorial/UsingJava.scala
@@ -0,0 +1,24 @@
+package com.yarenty.tutorial
+
+import java.text.DateFormat
+import java.text.DateFormat._
+import java.util.{Date, Locale}
+
+/**
+ * Example of using java inside scala program.
+ * Created by yarenty on 24/04/15.
+ */
+object UsingJava {
+
+  def main(args: Array[String]) {
+
+    val now = new Date
+    val df = DateFormat.getDateInstance(LONG, Locale.FRANCE)
+
+    println(df format now) // this is equivalent to:
+    println(df.format(now))
+
+  }
+
+
+}
