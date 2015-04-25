143ad947ee410ff2a09c872fda1c7466fabf9d15
yarenty <yarenty@gmail.com>
2015-04-25 12:08:07 +0100

    start with programming in scala

---
+++ src/main/scala/com/yarenty/examples/SimpleApplication.scala
@@ -0,0 +1,17 @@
+package com.yarenty.examples
+
+/**
+ * Yep - there are no Application traits anymore ;-(
+ * Created by yarenty on 25/04/15.
+ */
+object SimpleApplication {
+
+  def main(args: Array[String]) {
+
+    println( """|Hello World!
+               |This is cool
+               |formated String
+             """.stripMargin)
+  }
+
+}
