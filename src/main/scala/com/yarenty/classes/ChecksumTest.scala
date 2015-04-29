c767dad5b4800fc4bfebabd84ffc201868eb06ea
yarenty <yarenty@gmail.com>
2015-04-29 17:19:00 +0100

    classes example

--- src/main/scala/com/yarenty/classes/ChecksumTest.scala
+++ src/main/scala/com/yarenty/classes/ChecksumTest.scala
@@ -1,13 +1,19 @@
 package com.yarenty.classes
 
+
 /**
  * Created by yarenty on 29/04/15.
  */
-class ChecksumTest {
+object ChecksumTest {
 
   def main(args: Array[String]) {
     val acc = new ChecksumAccumulator
 
+    val b: Byte = 256.toByte
+    acc.add(b)
+
+    println(acc.checksum())
+    println(ChecksumAccumulator.calculate("Jaro"))
 
   }
 }
