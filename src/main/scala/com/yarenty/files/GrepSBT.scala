cfb6336fffef2417eb707e742a2143b6bad0478d
yarenty <yarenty@gmail.com>
2015-05-01 11:08:05 +0100

    return lists

--- src/main/scala/com/yarenty/files/GrepSBT.scala
+++ src/main/scala/com/yarenty/files/GrepSBT.scala
@@ -28,4 +28,52 @@ object GrepSBT extends App {
 
   grep(".*scala.*")
 
+  //Two changes:
+  // + there are {} instead () - so no need to use ; sign
+  // + as trim is expensive - here is used only once
+  def grepNoTrim(pattern: String) = {
+    println(" filtering inside head and pattern matching :")
+    for {file <- filesHere
+         if file.isFile
+         if file.getName.endsWith("sbt")
+         line <- fileLines(file)
+         trimmed = line.trim
+         if trimmed.matches(pattern)
+    }
+      println(file + ": " + trimmed)
+  }
+
+  grepNoTrim(".*scala.*")
+
+
+  println(" and here example of creating list/arrays from input")
+
+  def filesSubList(files: Array[File]) = {
+    for {file <- files
+         if file.isFile
+         if file.getName.endsWith("sbt")
+    }
+      yield file
+  }
+
+  println(filesSubList(filesHere))
+
+  def getGreppedLines(pattern: String) = {
+    println(" filtering inside head and pattern matching :")
+    for {file <- filesHere
+         if file.isFile
+         if file.getName.endsWith("sbt")
+         line <- fileLines(file)
+         trimmed = line.trim
+         if trimmed.matches(pattern)
+    }
+      yield trimmed
+  }
+
+  val lines = getGreppedLines(".*scala.*")
+
+  lines foreach println
+
+
+
 }
