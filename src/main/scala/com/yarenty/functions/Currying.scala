6a837ad06fd7a594481b840114141aee791285c9
yarenty <yarenty@gmail.com>
2015-05-05 12:24:01 +0100

    currying...

---
+++ src/main/scala/com/yarenty/functions/Currying.scala
@@ -0,0 +1,22 @@
+package com.yarenty.functions
+
+/**
+ * Created by yarenty on 05/05/15.
+ */
+object Currying extends App {
+
+  def plainOldSum(x: Int, y: Int) = x + y
+
+  def curriedSum(x: Int)(y: Int) = x + y
+
+
+  println(plainOldSum(1, 2))
+
+  println(curriedSum(1)(2))
+
+  val onePlus = curriedSum(1)_
+
+  println(onePlus(2))
+
+
+}
