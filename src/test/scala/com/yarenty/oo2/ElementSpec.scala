b3d3154718ecce6cb929c0f463949cd8cdd3d916
yarenty <yarenty@gmail.com>
2015-05-08 12:28:54 +0100

    small check on should be ..

--- src/test/scala/com/yarenty/oo2/ElementSpec.scala
+++ src/test/scala/com/yarenty/oo2/ElementSpec.scala
@@ -13,12 +13,15 @@ class ElementSpec extends FlatSpec with ShouldMatchers {
     val e = elem('x', 3, 4)
     info(" width is 3")
     assert(e.width == 3)
+    e.width should be_==(5) //not working ;-(
   }
+
   it should " height equal to passed value" in {
     val e = elem('x', 3, 4)
     info("height is 4 ")
     assert(e.height == 4)
   }
+
   it should " throw IAE if passed negative width" in {
     intercept[IllegalArgumentException] {
       info("for width 2 - no exception")
