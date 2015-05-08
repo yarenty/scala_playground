1c866420e9d719a1ee0fdf211f5ae956dc7fcc47
yarenty <yarenty@gmail.com>
2015-05-08 12:06:18 +0100

    BDD with FlatSpec

---
+++ src/test/scala/com/yarenty/oo2/ElementSpec.scala
@@ -0,0 +1,32 @@
+package com.yarenty.oo2
+
+import com.yarenty.oo2.Element.elem
+import org.scalatest.FlatSpec
+import org.specs2.matcher.ShouldMatchers
+
+/**
+ * Created by yarenty on 08/05/15.
+ */
+class ElementSpec extends FlatSpec with ShouldMatchers {
+
+  "A Uniform Element" should "have a width equal to the passed value" in {
+    val e = elem('x', 3, 4)
+    info(" width is 3")
+    assert(e.width == 3)
+  }
+  it should " height equal to passed value" in {
+    val e = elem('x', 3, 4)
+    info("height is 4 ")
+    assert(e.height == 4)
+  }
+  it should " throw IAE if passed negative width" in {
+    intercept[IllegalArgumentException] {
+      info("for width 2 - no exception")
+      val e = elem('x', 2, 4)
+      info("for width -2 - exception")
+      val e2 = elem('x', -2, 4)
+    }
+
+  }
+
+}
