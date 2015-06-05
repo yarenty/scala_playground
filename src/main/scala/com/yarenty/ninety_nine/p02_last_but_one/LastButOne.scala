8300e431c855b89c865ca240e8f655181c58089a
yarenty <yarenty@gmail.com>
2015-06-05 14:40:21 +0100

    99 p2

---
+++ src/main/scala/com/yarenty/ninety_nine/p02_last_but_one/LastButOne.scala
@@ -0,0 +1,28 @@
+package com.yarenty.ninety_nine.p02_last_but_one
+
+/**
+ * Find last but one element
+ * Created by yarenty on 05/06/15.
+ */
+object LastButOne {
+
+  def last(lx: List[Int]): Int = lx match {
+    case Nil => 0
+    case l :: Nil => 0
+    case l :: l2 :: Nil => l
+    case l :: l2 :: lx => last(l2 :: lx)
+  }
+
+  def lastReverse(lx: List[Int]): Int = lx.reverse.tail.head
+
+
+  def main(args: Array[String]) {
+    val l = List(1, 2, 3, 4, 5, 6, 7, 8)
+
+    println(last(l))
+
+    println(lastReverse(l))
+  }
+
+
+}
