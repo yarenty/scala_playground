ce808721c33aa64f27bffb1858aae4c4756dff37
yarenty <yarenty@gmail.com>
2015-06-17 11:36:21 +0100

    p11-p15 encode/decode/duplicate

--- src/main/scala/com/yarenty/ninety_nine/p10_run_length/SublistLength.scala
+++ src/main/scala/com/yarenty/ninety_nine/p10_run_length/SublistLength.scala
@@ -6,7 +6,6 @@ package com.yarenty.ninety_nine.p10_run_length
 object SublistLength {
 
 
-
   def pack[A](lx: List[A]): List[List[A]] = {
     if (lx.isEmpty) List(List())
     else {
@@ -19,15 +18,28 @@ object SublistLength {
 
   }
 
-  def encode[A](l:List[List[A]]): List[(A,Int)] = l match {
+  def encode[A](l: List[List[A]]): List[(A, Int)] = l match {
     case List() => Nil
-    case x :: l => (x.head, x.length) :: encode (l)
+    case x :: l => (x.head, x.length) :: encode(l)
   }
 
-  def encodeMap[A](l:List[List[A]]): List[(A,Int)] = {
+  def encodeMap[A](l: List[List[A]]): List[(A, Int)] = {
     l map { e => (e.head, e.length) }
   }
 
+
+  def decode[A](lx: List[(A, Int)]): List[A] = {
+    lx flatMap (e => List.fill(e._2)(e._1))
+  }
+
+
+  def duplicate[A](lx: List[A]): List[A] = {
+    lx flatMap (e => List(e, e))
+  }
+
+  def duplicateN[A](n: Int, lx: List[A]): List[A] = {
+    lx flatMap (e => List.fill(n)(e))
+  }
 
   def main(args: Array[String]) {
 
@@ -39,6 +51,14 @@ object SublistLength {
 
     println(e)
     println(encodeMap(p))
+
+    val d = decode(e)
+    println(d)
+
+    println(duplicate(d))
+
+    println(duplicateN(3, d))
+
   }
 
 
