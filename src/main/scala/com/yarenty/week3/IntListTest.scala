7064fbba28e5acc6873eabf0055fc9bbc3edc735
yarenty <yarenty@gmail.com>
2017-05-17 14:23:45 +0100

    Lists

--- src/main/scala/com/yarenty/week3/IntListTest.scala
+++ src/main/scala/com/yarenty/week3/IntListTest.scala
@@ -1,15 +1,32 @@
 package com.yarenty.week3
 
+import java.util.NoSuchElementException
+
 /**
   * Created by yarenty on 17/05/2017.
   */
-object IntListTest extends App{
+object IntListTest extends App {
 
 }
 
 
-trait List[T]
+trait List[T] {
+  def isEmpty: Boolean
 
-class Cons[T](val head:T, val tail: List[T]) extends List[T]
+  def head: T
 
-class Nil[T] extends List[T]
+  def tail: List[T]
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  override def isEmpty: Boolean = false
+}
+
+class Nil[T] extends List[T] {
+  override def isEmpty: Boolean = true
+
+  override def head: Nothing = throw new NoSuchElementException("Nil.head")
+
+  override def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+
+}
