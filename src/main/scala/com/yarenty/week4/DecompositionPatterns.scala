1ae7c11f67f4a7374f59e63c742bdf27ad3fff98
yarenty <yarenty@gmail.com>
2017-05-24 12:27:01 +0100

    decompositions different approaches

--- src/main/scala/com/yarenty/week4/DecompositionPatterns.scala
+++ src/main/scala/com/yarenty/week4/DecompositionPatterns.scala
@@ -4,17 +4,39 @@ package com.yarenty.week4
   * Created by yarenty on 24/05/2017.
   */
 object DecompositionPatterns extends App {
-  
-  trait Expr
-  case class Number(n:Int) extends Expr
-  case class Sum(e1:Expr,e2:Expr) extends Expr
 
-  
-  def eval(e:Expr):Int = {
-    e match {
-      case Number(n) => n
-      case Sum(e1,e2) => eval(e1) + eval(e2)
-    }
+  trait Expr {
+    def eval: Int =
+      this match {
+        case Number(n) => n
+        case Sum(e1, e2) => e1.eval + e2.eval
+        case Prod(e1, e2) => e1.eval * e2.eval
+      }
+
+    def show: String =
+      this match {
+        case Number(n) => n.toString
+        case Sum(e1, e2) => e1.show + " + " + e2.show
+        case Prod(e1:Sum, e2:Sum) => "(" + e1.show + ") * (" + e2.show + ")"
+        case Prod(e1:Sum, e2) => "(" + e1.show + ") * " + e2.show + ""
+        case Prod(e1, e2:Sum) => "" + e1.show + " * (" + e2.show + ")"
+        case Prod(e1, e2) => e1.show + " * " + e2.show 
+      }
   }
+
+  case class Number(n: Int) extends Expr
+
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+  case class Prod(e1: Expr, e2: Expr) extends Expr
+
+
+  val e = Sum(Number(4), Number(1))
   
+  println(e.show)
+  println(e.eval)
+
+  val e2 = Prod(e, Number(2))
+  println(e2.show)
+  println(e2.eval)
+
 }
