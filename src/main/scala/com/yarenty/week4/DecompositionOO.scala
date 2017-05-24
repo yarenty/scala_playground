72bef9beeb2fb8278f9256a6aa8110e149446b11
yarenty <yarenty@gmail.com>
2017-05-24 10:52:25 +0100

    decompositions different approaches

---
+++ src/main/scala/com/yarenty/week4/DecompositionOO.scala
@@ -0,0 +1,23 @@
+package com.yarenty.week4
+
+/**
+  * Simple Object Oriented Decomposition
+  *
+  *
+  * issues: 
+  *  - add new method - define in all childs
+  *  - a * b + a * c  -> a * ( b + c )
+  *
+  * Created by yarenty on 24/05/2017.
+  */
+trait Expr {
+  def eval: Int
+}
+
+class Number(n: Int) extends Expr {
+  override def eval: Int = n
+}
+
+class Sum(e1: Expr, e2: Expr) extends Expr {
+  override def eval: Int = e1.eval + e2.eval
+}
---
+++ src/main/scala/com/yarenty/week4/DecompositionSimple.scala
@@ -0,0 +1,58 @@
+package com.yarenty.week4
+
+/**
+  * Created by yarenty on 23/05/2017.
+  */
+trait Expr {
+  
+  //classifications
+  def isNumber: Boolean
+
+  def isSum: Boolean
+
+  
+  //accessors
+  def numValue: Int
+
+  def leftOp: Expr
+
+  def rigthOp: Expr
+
+}
+
+
+class Number(n: Int) extends Expr {
+  override def isNumber: Boolean = true
+
+  override def isSum: Boolean = false
+
+  override def numValue: Int = n
+
+  override def leftOp: Expr = throw new Error("number left op")
+
+  override def rigthOp: Expr = throw new Error("number right op")
+}
+
+
+class Sum(e1:Expr, e2:Expr) extends Expr {
+  override def isNumber: Boolean = false
+
+  override def isSum: Boolean = true
+
+  override def numValue: Int = throw new Error("sum value")
+
+  override def leftOp: Expr = e1
+
+  override def rigthOp: Expr = e2
+}
+
+
+object Test extends App {
+  
+  def eval(e:Expr):Int  =  {
+    if (e.isNumber) e.numValue
+    else if (e.isSum) eval(e.leftOp) + eval(e.rigthOp)
+    else throw new Error("Unknown op")
+  }
+  
+}
