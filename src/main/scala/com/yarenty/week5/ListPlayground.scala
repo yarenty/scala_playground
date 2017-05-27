e080b74448a9c713ef998903fb9cdbcc5af41da8
yarenty <yarenty@gmail.com>
2017-05-27 13:29:52 +0100

    MAP

--- src/main/scala/com/yarenty/week5/ListPlayground.scala
+++ src/main/scala/com/yarenty/week5/ListPlayground.scala
@@ -27,8 +27,33 @@ object ListPlayground extends App {
   }
 
 
-  def removeAt2[T](xs: List[T], n: Int): List[T] = (xs take n) ::: (xs drop n+1)
-  
+  def removeAt2[T](xs: List[T], n: Int): List[T] = (xs take n) ::: (xs drop n + 1)
+
+
+  def scaleList(xs: List[Double], factor: Double): List[Double] = xs match {
+    case Nil => xs
+    case y :: ys => y * factor :: scaleList(ys, factor)
+
+  }
+
+  def map[T](xs: List[T])(f: T => T): List[T] = xs match {
+    case Nil => xs
+    case y :: ys => f(y) :: map(ys)(f)
+  }
+
+
+  def scaleList2(xs: List[Double], factor: Double): List[Double] = map(xs)(x => x * factor)
+
+
+
+
+  def squareList(xs: List[Double]): List[Double] = xs match {
+    case Nil => xs
+    case y :: ys => y * y :: squareList(ys)
+
+  }
+
+  def squareList2(xs: List[Double]): List[Double] = map(xs)(x => x * x)
 
 
   val l = List(1, 2, 3, 4, 5)
@@ -39,9 +64,18 @@ object ListPlayground extends App {
   show(l.updated(3, 66))
   show(concat(l, l2))
   show(reverse(l))
-  show(removeAt(l,3))
-  show(removeAt(l,8))
-  show(removeAt2(l,3))
-  show(removeAt2(l,8))
+  show(removeAt(l, 3))
+  show(removeAt(l, 8))
+  show(removeAt2(l, 3))
+  show(removeAt2(l, 8))
 
+
+  val dl = List(1.0, 4.0, -8.0, 1.2, 453.3)
+  show(scaleList(dl, 2.3))
+  show(scaleList2(dl, 2.3))
+
+
+
+  show(squareList(dl))
+  show(squareList2(dl))
 }
