84a834d2549909bd343f8b45f1a0cbfdbb5b5f85
yarenty <yarenty@gmail.com>
2017-05-27 14:06:56 +0100

    FOld left and right

--- src/main/scala/com/yarenty/week5/ListPlayground.scala
+++ src/main/scala/com/yarenty/week5/ListPlayground.scala
@@ -51,7 +51,7 @@ object ListPlayground extends App {
     case y :: ys => y * factor :: scaleList(ys, factor)
   }
 
-  def map[T,U](xs: List[T])(f: T => U): List[U] = xs match {
+  def map[T, U](xs: List[T])(f: T => U): List[U] = xs match {
     case Nil => Nil
     case y :: ys => f(y) :: map(ys)(f)
   }
@@ -82,50 +82,64 @@ object ListPlayground extends App {
     case y :: ys => if (y > 0) y :: posElems(ys) else posElems(ys)
   }
 
-  def posElems2(xs: List[Int]): List[Int] = xs filter( x=> x>0)
+  def posElems2(xs: List[Int]): List[Int] = xs filter (x => x > 0)
 
- val di = map(dl)(x => x.toInt)
+  val di = map(dl)(x => x.toInt)
   show(posElems(di))
   show(posElems2(di))
-  
-  val (p,n) = di.partition(x =>x>0)
+
+  val (p, n) = di.partition(x => x > 0)
   show(p)
   show(n)
-  show(di.takeWhile(x=>x>0))
-  show(di.dropWhile(x=>x>0))
-  
-  
-  
-  
-  def pack[T](xs:List[T]):List[List[T]] = xs match {
+  show(di.takeWhile(x => x > 0))
+  show(di.dropWhile(x => x > 0))
+
+
+  def pack[T](xs: List[T]): List[List[T]] = xs match {
     case Nil => Nil
-    case y::ys =>
-      val (l,r) =  xs.partition(z=>z==y)
+    case y :: ys =>
+      val (l, r) = xs.partition(z => z == y)
       l :: pack(r)
   }
 
-  def pack2[T](xs:List[T]):List[List[T]] = xs match {
+  def pack2[T](xs: List[T]): List[List[T]] = xs match {
     case Nil => Nil
-    case y::ys =>
-      val (l,r) =  xs.span(z=>z==y)
+    case y :: ys =>
+      val (l, r) = xs.span(z => z == y)
       l :: pack2(r)
   }
-  
-  
-  def encode[T](xs: List[List[T]]):List[(T,Int)] = xs match{
+
+
+  def encode[T](xs: List[List[T]]): List[(T, Int)] = xs match {
     case Nil => Nil
-    case y::ys => (y.head,y.length) :: encode(ys)
+    case y :: ys => (y.head, y.length) :: encode(ys)
   }
 
 
-  def encode2[T](xs: List[T]):List[(T,Int)] =  pack2(xs) map (ys => (ys.head, ys.length))
+  def encode2[T](xs: List[T]): List[(T, Int)] = pack2(xs) map (ys => (ys.head, ys.length))
 
 
-
-  val pp = List(1,2,1,1,1,3,3,3,4,4,4,1)
+  val pp = List(1, 2, 1, 1, 1, 3, 3, 3, 4, 4, 4, 1)
   show(pack(pp))
   show(pack2(pp))
   show(encode(pack2(pp)))
   show(encode2(pp))
+
+
+  //FOLDS
+
+  def sum(xs: List[Int]) = (0 :: xs) reduceLeft (_ + _)
+
+  def product(xs: List[Int]) = (1 :: xs) reduceLeft (_ * _)
+
+
+  def sum2(xs: List[Int]):Int = xs.foldLeft(0)(_ + _)
+
+  def product2(xs: List[Int]) = xs.foldLeft(1)(_ * _)
+  
+  println(sum(l))
+  println(sum2(l))
+  println(sum(l2))
+  println(sum(l2))
 
 }
