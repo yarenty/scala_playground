b26142de5d83c476ffe50b4226ddc891fe39bfa5
yarenty <yarenty@gmail.com>
2017-05-27 13:17:40 +0100

    implicits

--- src/main/scala/com/yarenty/week5/MergeSortParametarizedList.scala
+++ src/main/scala/com/yarenty/week5/MergeSortParametarizedList.scala
@@ -17,18 +17,44 @@ object MergeSortParametarizedList extends App {
     if (n == 0) xs
     else {
       
-      def merge2(xs: List[T], ys: List[T])(lt: (T,T) =>Boolean): List[T] = (xs, ys) match {
+      def merge2(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
         case (Nil, a) => ys
         case (a, Nil) => xs
-        case (x :: xxs, y :: yys) => if (lt(x, y)) x :: merge2(xxs, ys)(lt) else y :: merge2(xs, yys)(lt)
+        case (x :: xxs, y :: yys) => if (lt(x, y)) x :: merge2(xxs, ys) else y :: merge2(xs, yys)
       }
 
       val (fst, snd) = xs splitAt (n)
 
-      merge2(msort(fst)(lt), msort(snd)(lt))(lt)
+      merge2(msort(fst)(lt), msort(snd)(lt))
     }
 
   }
+
+
+
+
+
+  def msort2[T](xs: List[T])(implicit  ord:Ordering[T]): List[T] = {
+    val n = xs.length / 2
+
+    if (n == 0) xs
+    else {
+
+      def merge2(xs: List[T], ys: List[T]): List[T] = (xs, ys) match {
+        case (Nil, a) => ys
+        case (a, Nil) => xs
+        case (x :: xxs, y :: yys) => if (ord.lt(x, y)) x :: merge2(xxs, ys) else y :: merge2(xs, yys)
+      }
+
+      val (fst, snd) = xs splitAt (n)
+
+      merge2(msort2(fst), msort2(snd))
+    }
+
+  }
+
+
+
 
 
   val l = List(5, 4, 3, 2, 1, 7)
@@ -36,7 +62,20 @@ object MergeSortParametarizedList extends App {
   val l2 = List(6, 7, 8, 9, 10)
   show(l2)
   show(msort(l)( (x,y) => x<y))
+  
+  val fruits = List("Apple", "Pineaple", "Banana")
+  show(msort(fruits)( (x,y) => x.compareTo(y) <0))
 
+
+  show(msort2(l)(Ordering.Int))
+
+  show(msort2(fruits)(Ordering.String))
+
+
+
+  show(msort2(l))
+
+  show(msort2(fruits))
 }
 
 
