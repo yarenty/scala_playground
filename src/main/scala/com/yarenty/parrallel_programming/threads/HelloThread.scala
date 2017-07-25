52bedbdee684179c5ae01659e915a7c7c48ae757
yarenty <yarenty@gmail.com>
2017-07-25 12:00:35 +0100

    threads - overlapping

--- src/main/scala/com/yarenty/parrallel_programming/threads/HelloThread.scala
+++ src/main/scala/com/yarenty/parrallel_programming/threads/HelloThread.scala
@@ -12,6 +12,17 @@ class HelloThread extends Thread{
 }
 
 
+class HelloThread2 extends Thread{
+
+  override def run(): Unit = {
+
+    println("Hello")
+    println("World!!")
+  }
+}
+
+
+
 
 
 
@@ -25,6 +36,12 @@ object TTest extends App {
   t.join()
   println("c")
   println("d")
-  
-  
+
+
+  val t2  = new HelloThread2
+  val t3  = new HelloThread2
+  t2.start()
+  t3.start()
+  t2.join()
+  t3.join()  //random ;-)
 }
